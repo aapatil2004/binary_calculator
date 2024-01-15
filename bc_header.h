6a2f7866ba68db62209e480166bdea8afec3f036
aapatil2004 <144612451+aapatil2004@users.noreply.github.com>
2024-01-15 23:07:26 +0530

    Update bc_header.h

--- bc_header.h
+++ bc_header.h
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct node {
+typedef struct node
+{
+    struct node* next, *prev;
     int data;
-    struct node *next, *prev;
-} node;
+}node;
 
 typedef struct number {
+    int sign;
     node *front, *rear;
 } number;
 
@@ -14,9 +16,13 @@ void init(number *head);
 int isempty(number head);
 void insertLeft(number *head, int d);
 void insertRight(number *head, int d);
-void insertDecimalPoint(number *head);
+void insertDecimalPointR(number *head);
+void insertDecimalPointL(number *head);
 void insertAtIndex(number * head, int index, int data);
-void insertNegative(number *head);
+void insertNegativeL(number *head);
+void insertNegativeR(number *head);
+number deleteNegativeR(number head);
+number deleteR(number head);
 number reverse(number head);
 int count(number *head);
 void displayLR(number head);
